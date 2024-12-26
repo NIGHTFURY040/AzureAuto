aa4625e9ce71fbe71c2dbf5e0638c9cd69c61c13
NIGHTFURY040 <144832055+NIGHTFURY040@users.noreply.github.com>
2024-12-26 06:31:11 +0000

    Update TestRB19.ps1

--- TestRB19.ps1
+++ TestRB19.ps1
@@ -1,3 +1,3 @@
 Write-Output "This is test for sync jobs in azure automation"
 
-Write-host "this is to test the push-pull to check for merge error"
+Write-host "this is to test the push-pull to check for merge conflict"
